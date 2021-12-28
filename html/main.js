80e0900c7d07f2ea81796e32fe58a546c485350f
Lazy-ferret <a.veselov83@gmail.com>
2021-12-28 22:57:11 +0300

    Choose train markup and styles done

---
+++ html/main.js
@@ -0,0 +1,5 @@
+const pickerStart = datepicker('.Input_date_start')
+const pickerEnd = datepicker('.Input_date_end')
+const pickerDeparture = datepicker('.Input_departure_date')
+const pickerReturn = datepicker('.Input_return_date')
+
